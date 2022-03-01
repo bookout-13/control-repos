923f3fd3418ae1cf54faad542bb3e19ebaf2fbb0
bookout-13 <99298075+bookout-13@users.noreply.github.com>
2022-03-01 15:14:55 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
-    ensure => file,
+    ensure  => file,
     content => 'This is a readme, I need this to work',
-    owner => 'root',
+    owner   => 'root',
   }
 }
